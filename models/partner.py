7eff85f2560263bb6b23b6ed015573cc251079e9
Hasdi04 <hasdi@ozbgroup.com>
2022-08-02 09:39:41 +0800

    inherit

--- models/partner.py
+++ models/partner.py
@@ -3,5 +3,10 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    instructor= fields.Boolean(default=False)
+
+
+    instructor = fields.Boolean(default=False)
     sessions = fields.Many2many('opentutorial.session')
+
+    # teacher = fields.Selection([('Teacher1', 'Level 1'), ('Teacher2', 'Level 2')])
+
