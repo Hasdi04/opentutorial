f851b661c5dc53545f06229bd7c214e23ee9e2fa
Hasdi04 <hasdi@ozbgroup.com>
2022-08-02 16:54:26 +0800

    constrain done

--- models/session.py
+++ models/session.py
@@ -1,4 +1,9 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
+
 
 class session(models.Model):
     _name = 'opentutorial.session'
@@ -6,9 +11,10 @@ class session(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Char()
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.date.today())
     duration = fields.Float()
     seats = fields.Integer()
+    active = fields.Boolean(default=True)
 
     # related fields
     instructor = fields.Many2one('res.partner', domain = ['|', ('instructor', '=', True),
@@ -18,3 +24,49 @@ class session(models.Model):
     # one session have many courses
     # list of course will appear on courses field in session form
     attendees = fields.Many2many('res.partner')
+
+    # computed fields
+    taken_seats = fields.Char(compute='_taken_seats')
+
+    @api.depends('attendees', 'seats')
+    def _taken_seats(self):
+        _logger.info("----- %s -----", self)
+        for r in self:
+            _logger.info("----- seats: %s -----", r.seats)
+            _logger.info("----- attendees: %s -----", r.attendees)
+            if not r.seats:
+                r.taken_seats = "0.0%"
+            else:
+                x = 100 * len(r.attendees) / r.seats
+                r.taken_seats = str(x) + '%'
+
+
+    @api.onchange('attendees', 'seats')
+    def _check_seats(self):
+            if self.seats < 0:
+                return{
+                    'warning': {
+                        'title':'Invalid number',
+                        'message':'Put correct number'
+                    }
+                }
+            elif self.seats < len(self.attendees):
+                return {
+                    'warning' : {
+                        'title':"too many participants",
+                        'message' : "add more seats"
+                    }
+                }
+
+
+    @api.constrains('attendees','instructor')
+    def _check_instructor(self):
+        for r in self:
+            if r.instructor in r.attendees:
+                raise ValidationError('Instructor cannot be in attendees')
+
+
+
+
+
+
